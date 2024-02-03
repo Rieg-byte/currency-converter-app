2ee0f3fe4f5ec770f1a0b0542c2fab09ed1fc843
Lineked <lineked@yandex.ru>
2024-02-03 17:14:13 +0300

    Remove ResultStatus.kt

--- app/src/main/java/com/example/currencyconverterapp/util/ResultStatus.kt
+++
@@ -1,6 +0,0 @@
-package com.example.currencyconverterapp.util
-
-sealed class ResultStatus<T> (val data: T?, val message: String?) {
-    class Success<T>(data: T): ResultStatus<T>(data, null)
-    class Error<T>(message: String): ResultStatus<T>(null, message)
-}
