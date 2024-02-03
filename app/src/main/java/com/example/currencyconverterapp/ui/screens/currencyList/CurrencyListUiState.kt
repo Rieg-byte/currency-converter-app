f3794b9b0975f20273843872220c7091be057862
Lineked <lineked@yandex.ru>
2024-02-03 21:36:48 +0300

    removed unused parameters

--- app/src/main/java/com/example/currencyconverterapp/ui/screens/currencyList/CurrencyListUiState.kt
+++ app/src/main/java/com/example/currencyconverterapp/ui/screens/currencyList/CurrencyListUiState.kt
@@ -3,10 +3,5 @@ package com.example.currencyconverterapp.ui.screens.currencyList
 import com.example.currencyconverterapp.data.model.Currency
 
 data class CurrencyListUiState(
-    val amount: String = "",
-    val inputCurrency: String = "USD",
-    val outputCurrency: String = "RUB",
-    val convertedAmount: String = "0.0",
     val listOfCurrency: List<Currency> = emptyList(),
-    val currentRate: Double = 0.0
 )
