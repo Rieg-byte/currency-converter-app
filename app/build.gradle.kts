fac8d646d3c7d929b6638b9a7874475049bacf57
Lineked <lineked@yandex.ru>
2024-02-20 09:17:34 +0300

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,11 +57,11 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3:1.2.0")
     // viewmodel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     // retrofit
