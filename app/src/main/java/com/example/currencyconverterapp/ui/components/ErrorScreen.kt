c995a3c77c8b663e57798ee21a813b4884be841b
Lineked <lineked@yandex.ru>
2024-02-29 21:32:03 +0300

    moved in package components

---
+++ app/src/main/java/com/example/currencyconverterapp/ui/components/ErrorScreen.kt
@@ -0,0 +1,33 @@
+package com.example.currencyconverterapp.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.currencyconverterapp.R
+
+@Composable
+fun ErrorScreen(
+    onRepeat: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = stringResource(id = R.string.error))
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = onRepeat) {
+            Text(text = stringResource(id = R.string.repeat))
+        }
+    }
+}
