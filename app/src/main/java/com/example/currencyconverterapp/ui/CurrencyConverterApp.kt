e3f18f95ac79a08ec6108f238101774a75997d2c
Lineked <lineked@yandex.ru>
2024-02-29 21:34:30 +0300

    replaced from CurrencyListScreen to CurrencyConverterNavHost

--- app/src/main/java/com/example/currencyconverterapp/ui/CurrencyConverterApp.kt
+++ app/src/main/java/com/example/currencyconverterapp/ui/CurrencyConverterApp.kt
@@ -1,9 +1,9 @@
 package com.example.currencyconverterapp.ui
 
 import androidx.compose.runtime.Composable
-import com.example.currencyconverterapp.ui.screens.currencyList.CurrencyListScreen
+import com.example.currencyconverterapp.ui.navigation.CurrencyConverterNavHost
 
 @Composable
 fun CurrencyConverterApp() {
-    CurrencyListScreen()
+    CurrencyConverterNavHost()
 }
