6d35e9a0c467cc22d9eef8d4de134be8d74715b9
Lineked <lineked@yandex.ru>
2024-02-29 08:52:26 +0300

    add Compose Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,16 +67,17 @@ dependencies {
     // retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     // hilt
     implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
     // serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.50")
+    // Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
