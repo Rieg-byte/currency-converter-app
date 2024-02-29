8ec7c93eadf243cb486f0a81213f934aaafe055b
Lineked <lineked@yandex.ru>
2024-02-29 21:32:45 +0300

    added hilt-navigation-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     // Compose Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
