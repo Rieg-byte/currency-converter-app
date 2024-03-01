5e5886d59185e7e6fb80c7ad86abb881e8bf9d7e
Lineked <lineked@yandex.ru>
2024-03-01 20:55:51 +0300

    Added color for diff

--- app/src/main/java/com/example/currencyconverterapp/ui/screens/currencyList/CurrencyListScreen.kt
+++ app/src/main/java/com/example/currencyconverterapp/ui/screens/currencyList/CurrencyListScreen.kt
@@ -6,13 +6,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
@@ -24,9 +28,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -133,11 +139,18 @@ fun CurrencyCard(
     currency: Currency,
     onNavigateToConverter: (String) -> Unit = {}
 ){
+    val diff = currency.value - currency.previous
+    val color = when {
+        diff > 0 -> Color(0xFF87E455)
+        diff < 0 -> Color(0xFFF44336)
+        else -> Color(0xFFD5D5D5)
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
             .clickable { onNavigateToConverter(currency.charCode) }
+            .height(60.dp)
     ) {
         Row(
             modifier = Modifier
@@ -164,19 +177,36 @@ fun CurrencyCard(
             }
             Row(
                 modifier = Modifier.weight(1f),
-                horizontalArrangement = Arrangement.End
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
+                    modifier = Modifier.weight(1f),
+                    textAlign = TextAlign.End,
                     fontSize = 24.sp,
                     fontWeight = FontWeight.SemiBold,
                     text = "%.2f".format(currency.value)
                 )
-                Spacer(modifier = Modifier.width(4.dp))
-                Text(
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Normal,
-                    text = "(%.2f)".format(currency.value - currency.previous)
-                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Card(
+                    modifier = Modifier.weight(1f),
+                    colors = CardDefaults.cardColors(
+                        containerColor = color
+                    )
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            textAlign = TextAlign.Center,
+                            fontSize = 24.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Normal,
+                            text = "%.2f".format(diff)
+                        )
+                    }
+                }
             }
         }
     }
@@ -184,7 +214,7 @@ fun CurrencyCard(
 
 @Preview(showBackground = true)
 @Composable
-fun CurrencyCardPreview(){
+fun CurrencyCardPreview1(){
     CurrencyCard(
         currency = Currency(
             id = "2",
@@ -193,7 +223,39 @@ fun CurrencyCardPreview(){
             nominal = 1,
             name = "Американский доллар",
             value = 90.11,
-            previous = 92.01
+            previous = 90.10
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CurrencyCardPreview2(){
+    CurrencyCard(
+        currency = Currency(
+            id = "2",
+            numCode = "011",
+            charCode = "USD",
+            nominal = 1,
+            name = "Американский доллар",
+            value = 90.11,
+            previous = 90.11
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CurrencyCardPreview3(){
+    CurrencyCard(
+        currency = Currency(
+            id = "2",
+            numCode = "011",
+            charCode = "USD",
+            nominal = 1,
+            name = "Американский доллар",
+            value = 90.11,
+            previous = 92.3
         )
     )
 }
