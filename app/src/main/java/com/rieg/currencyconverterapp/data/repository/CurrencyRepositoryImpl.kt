62e22b57e63b97bc2c3f7991a8e31025aaed09d6
Lineked <lineked@yandex.ru>
2024-05-27 22:55:05 +0300

    minor changes

--- app/src/main/java/com/rieg/currencyconverterapp/data/repository/CurrencyRepositoryImpl.kt
+++ app/src/main/java/com/rieg/currencyconverterapp/data/repository/CurrencyRepositoryImpl.kt
@@ -1,17 +1,21 @@
 package com.rieg.currencyconverterapp.data.repository
 
-import com.rieg.currencyconverterapp.data.model.Currency
-import com.rieg.currencyconverterapp.data.model.CurrencyData
+import com.rieg.currencyconverterapp.data.mappers.toCurrency
+import com.rieg.currencyconverterapp.data.mappers.toCurrencyData
 import com.rieg.currencyconverterapp.data.remote.CurrencyRemoteDataSource
+import com.rieg.currencyconverterapp.domain.models.Currency
+import com.rieg.currencyconverterapp.domain.models.CurrencyData
+import com.rieg.currencyconverterapp.domain.repository.CurrencyRepository
 import javax.inject.Inject
 
-class CurrencyRepositoryImpl @Inject constructor(private val currencyRemoteDataSource: CurrencyRemoteDataSource) : CurrencyRepository {
+class CurrencyRepositoryImpl @Inject constructor(private val currencyRemoteDataSource: CurrencyRemoteDataSource) :
+    CurrencyRepository {
     override suspend fun getCurrencyData(): CurrencyData {
-        return currencyRemoteDataSource.getCurrencyData()
+        return currencyRemoteDataSource.getCurrencyDataResponse().toCurrencyData()
     }
 
     override suspend fun getCurrency(charCode: String): Currency? {
-        val currencyData = currencyRemoteDataSource.getCurrencyData()
-        return currencyData.currencies[charCode]
+        val currencyData = currencyRemoteDataSource.getCurrencyDataResponse()
+        return currencyData.currencies[charCode]?.toCurrency()
     }
 }
