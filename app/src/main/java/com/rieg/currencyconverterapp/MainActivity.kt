1e703a979474d6f091c4dbde7d821a7537045408
Lineked <lineked@yandex.ru>
2024-06-06 00:58:58 +0300

    new imports

--- app/src/main/java/com/rieg/currencyconverterapp/MainActivity.kt
+++ app/src/main/java/com/rieg/currencyconverterapp/MainActivity.kt
@@ -3,10 +3,16 @@ package com.rieg.currencyconverterapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.rieg.currencyconverterapp.domain.models.ThemeMode
 import com.rieg.currencyconverterapp.ui.CurrencyConverterApp
 import com.rieg.currencyconverterapp.ui.theme.CurrencyConverterAppTheme
 import dagger.hilt.android.AndroidEntryPoint
