280ffd985d860d6ec9d07542fa80000447142a27
Lineked <lineked@yandex.ru>
2024-06-06 00:57:19 +0300

    added SettingsViewModel

---
+++ app/src/main/java/com/rieg/currencyconverterapp/presentation/settings/SettingsViewModel.kt
@@ -0,0 +1,30 @@
+package com.rieg.currencyconverterapp.presentation.settings
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rieg.currencyconverterapp.domain.models.ThemeMode
+import com.rieg.currencyconverterapp.domain.repository.SettingsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(private val settingsRepository: SettingsRepository): ViewModel() {
+    val settingsUiState: StateFlow<SettingsUiState> = settingsRepository.themeMode.map { themeMode ->
+        SettingsUiState(themeMode = themeMode)
+    }.stateIn(
+        scope = viewModelScope,
+        started = WhileSubscribed(5.seconds.inWholeMilliseconds),
+        initialValue = SettingsUiState(themeMode = ThemeMode.SYSTEM.name)
+    )
+
+    fun updateThemeMode(themeMode: ThemeMode) = viewModelScope.launch {
+        settingsRepository.setThemeMode(themeMode)
+    }
+
+}
