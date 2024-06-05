ab9932d48b5a43780c718b3c6c5994f27d338d89
Lineked <lineked@yandex.ru>
2024-06-05 14:09:18 +0300

    add color down and rise

--- app/src/main/java/com/rieg/currencyconverterapp/ui/theme/Theme.kt
+++ app/src/main/java/com/rieg/currencyconverterapp/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.rieg.currencyconverterapp.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -10,10 +11,19 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+
+val ColorScheme.down: Color
+    @Composable
+    get() = Color(0xFFE03D31)
+
+val ColorScheme.rise: Color
+    @Composable
+    get() = Color(0xFF4CAF50)
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
