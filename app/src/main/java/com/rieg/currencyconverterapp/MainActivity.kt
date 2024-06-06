eb004032a26228c627fd3eb531a6ad45c8d138fd
Lineked <lineked@yandex.ru>
2024-06-06 11:14:38 +0300

    changed getting uiState

--- app/src/main/java/com/rieg/currencyconverterapp/MainActivity.kt
+++ app/src/main/java/com/rieg/currencyconverterapp/MainActivity.kt
@@ -3,27 +3,41 @@ package com.rieg.currencyconverterapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.rieg.currencyconverterapp.domain.models.ThemeMode
 import com.rieg.currencyconverterapp.ui.CurrencyConverterApp
 import com.rieg.currencyconverterapp.ui.theme.CurrencyConverterAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val mainActivityViewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var mainActivityUiState: MainActivityUiState by mutableStateOf(MainActivityUiState(themeMode = ThemeMode.SYSTEM.name))
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainActivityViewModel.mainActivityUiState
+                    .onEach { mainActivityUiState = it }
+                    .collect()
+            }
+        }
         setContent {
-            val mainActivityViewModel: MainActivityViewModel = hiltViewModel()
-            val mainActivityUiState by mainActivityViewModel.mainActivityUiState.collectAsState()
             CurrencyConverterAppTheme(darkTheme = shouldUseDarkTheme(mainActivityUiState)) {
                 // A surface container using the 'background' color from the theme
                 Surface(
